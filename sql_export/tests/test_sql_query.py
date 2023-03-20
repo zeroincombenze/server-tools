6360219c3755bc0f9e3060cad0e224a52d120448
zeroincombenze <info@shs-av.com>
2023-03-20 12:12:07 +0100

    [NEW] Initial setup 12.0

--- sql_export/tests/test_sql_query.py
+++ sql_export/tests/test_sql_query.py
@@ -1,65 +1,72 @@
-#    Author: Florian da Costa
-#    Copyright 2015 Akretion
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distnaributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# Copyright (C) 2015 Akretion (<http://www.akretion.com>)
+# @author: Florian da Costa
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 import base64
-from openerp.tests.common import TransactionCase
-from openerp.osv.orm import except_orm
+from odoo.tests.common import TransactionCase, post_install
+from odoo.exceptions import UserError
+from odoo import fields
 
 
+@post_install(True)
 class TestExportSqlQuery(TransactionCase):
 
     def setUp(self):
         super(TestExportSqlQuery, self).setUp()
-        query_vals = {
-            'name': 'test',
-            'query': "SELECT name, street FROM res_partner;"
-        }
-        self.sql_model = self.registry('sql.export')
-        self.query_id = self.sql_model.create(
-            self.cr,
-            self.uid,
-            query_vals)
+        self.sql_export_obj = self.env['sql.export']
+        self.wizard_obj = self.env['sql.file.wizard']
+        self.sql_report_demo = self.env.ref('sql_export.sql_export_partner')
 
     def test_sql_query(self):
-        test = self.sql_model.export_sql_query(
-            self.cr, self.uid, [self.query_id])
-        wizard = self.registry('sql.file.wizard').browse(
-            self.cr, self.uid, test['res_id'])
-        export = base64.b64decode(wizard.file)
+        wizard = self.wizard_obj.create({
+            'sql_export_id': self.sql_report_demo.id,
+        })
+        wizard.export_sql()
+        export = base64.b64decode(wizard.binary_file).decode('utf-8')
         self.assertEqual(export.split(';')[0], 'name')
         self.assertTrue(len(export.split(';')) > 6)
 
-    def test_prohibited_queries_creation(self):
+    def test_prohibited_queries(self):
         prohibited_queries = [
             "upDaTe res_partner SET name = 'test' WHERE id = 1",
-            "DELETE FROM res_partner;",
-            "  DELETE FROM res_partner   ;",
+            "DELETE FROM sql_export WHERE name = 'test';",
+            "  DELETE FROM sql_export WHERE name = 'test'   ;",
             """DELETE
             FROM
-            res_partner
+            sql_export
+            WHERE name = 'test'
             """,
         ]
         for query in prohibited_queries:
-            with self.assertRaises(except_orm):
-                self.sql_model.create(
-                    self.cr, self.uid,
-                    {'name': 'test_prohibited',
-                     'query': query})
-        ok_query = {
-            'name': 'test ok',
-            'query': "SELECT create_date FROM res_partner"
-        }
-        query_id = self.sql_model.create(self.cr, self.uid, ok_query)
-        self.assertIsNotNone(query_id)
+            with self.assertRaises(UserError):
+                sql_export = self.sql_export_obj.create({
+                    'name': 'test_prohibited',
+                    'query': query})
+                sql_export.button_validate_sql_expression()
+
+    def test_authorized_queries(self):
+        authorized_queries = [
+            "SELECT create_date FROM res_partner",
+        ]
+
+        for query in authorized_queries:
+            sql_export = self.sql_export_obj.create({
+                'name': 'test_authorized',
+                'query': query})
+            sql_export.button_validate_sql_expression()
+            self.assertEqual(
+                sql_export.state, 'sql_valid',
+                "%s is a valid request" % (query))
+
+    def test_sql_query_with_params(self):
+        query = self.env.ref('sql_export.sql_export_partner_with_variables')
+        categ_id = self.env.ref('base.res_partner_category_0').id
+        wizard = self.wizard_obj.create({
+            'sql_export_id': query.id,
+            'x_date': fields.Date.today(),
+            'x_id': 1,
+            'x_partner_categ_ids': [(6, 0, [categ_id])]
+        })
+        wizard.export_sql()
+        export = base64.b64decode(wizard.binary_file)
+        self.assertTrue(export)
